87ef7e256fcfff7ce8d1ab925fd68a34d3a3b4c4
leeyelan <yr00868@sookmyung.ac.kr>
2020-11-12 16:26:47 +0900

    5th

--- main.c
+++ main.c
@@ -1,19 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_image(int a[5][5])
+{
+	int i,j;
+	for(i=0; i<5; i++)
+	{
+		for(j=0; j<5; j++)
+	{
+			printf("%d",a[i][j]);
+	
+		}
+	printf("\n");
+	}
+	
+}
+
+void brighten_image(int a[5][5])
+{
+	int i,j;
+	int *ptr;
+	
+	ptr = &a[0][0];
+	for(i=0; i<5; i++)
+	 
+	 for(j=0; j<5; j++)
+	 {
+	 	*ptr += 10;
+	 	ptr++;
+	 	
+	 }
+}
+
+
+
 int main(void)
 {
-
-int i = 100;
-int *p = &i;
-int **q = &p;
-
-*p = 200;
-printf("i=%d, *p = %d, **q = %d\n",i,*p,**q);
-
-**q = 300;
-printf("i = %d, *p = %d, **q = %d\n",i,*p,**q);
-
-return 0;
-
+	int image[5][5] ={
+	
+	{10,20,30,40,50},
+	{10,20,30,40,50},
+	{10,20,30,40,50},
+	{10,20,30,40,50},
+	{10,20,30,40,50},
+	
+	};
+	
+	print_image(image);
+	brighten_image(image);
+	print_image(image);
+	return 0;
 }
