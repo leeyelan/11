8fdab45944f9ab2c3213fde8381b50210d543e93
leeyelan <yr00868@sookmyung.ac.kr>
2020-11-12 16:27:35 +0900

    4th

--- main.c
+++ main.c
@@ -1,53 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_image(int a[5][5])
+char *proverb = "All that gliters is not gold.";
+
+void setPointer(char **q)
 {
-	int i,j;
-	for(i=0; i<5; i++)
-	{
-		for(j=0; j<5; j++)
-	{
-			printf("%d",a[i][j]);
-	
-		}
-	printf("\n");
-	}
-	
+    *q = proverb;	
 }
-
-void brighten_image(int a[5][5])
-{
-	int i,j;
-	int *ptr;
-	
-	ptr = &a[0][0];
-	for(i=0; i<5; i++)
-	 
-	 for(j=0; j<5; j++)
-	 {
-	 	*ptr += 10;
-	 	ptr++;
-	 	
-	 }
-}
-
-
 
 int main(void)
 {
-	int image[5][5] ={
+	char *p = "zzz";
+	setPointer(&p);
+	printf("%s\n",p);
 	
-	{10,20,30,40,50},
-	{10,20,30,40,50},
-	{10,20,30,40,50},
-	{10,20,30,40,50},
-	{10,20,30,40,50},
-	
-	};
-	
-	print_image(image);
-	brighten_image(image);
-	print_image(image);
-	return 0;
+	return 0 ;
 }
+
