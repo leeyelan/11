592b92e6dcac2822e94afb7a16950f93b8517da6
leeyelan <yr00868@sookmyung.ac.kr>
2020-11-12 15:36:15 +0900

    2nd

--- main.c
+++ main.c
@@ -3,21 +3,18 @@
 
 int main(void)
 {
-	char *pc;
-	int *pi;
-	double *pd;
-	
-	pc = (char*)10000;
-	pi = (int*)10000;
-	pd = (double*)10000;
-	
-	printf("Before increasing : pc =%d, pi = %d, pd = %d\n",pc,pi,pd);
-	
-	pc++;
-	pi++;
-	pd++;
-	
-	printf("After increasing : pc =%d, pi =%d, pd =%d\n",pc,pi,pd);
-	
-	return 0;
+
+int i = 10;
+int *ptr = &i;
+
+printf("i = %d, pi: %p\n",i,ptr);
+(*ptr)++;
+
+printf("i = %d, pi: %p\n",i,ptr);
+*ptr++;
+
+printf("i = %d, pi: %p\n",i,ptr);
+
+return 0;
+
 }
